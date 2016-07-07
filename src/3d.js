73f603fed162dbb329c0d70a374a2dacf9759c53
David Li <david.li@tum.de>
2016-07-07 10:51:03 +0200

    restoring encapsulation, now across multiple files

--- src/3d.js
+++ src/3d.js
@@ -1,213 +1,228 @@
+"use strict";
 
-// project-wide variables
-var scene, camera, renderer, stats, controls, manager;
+// File 1: exports _vw into AntMe, which is the interface to all graphic objects
+//         and the init function to start everything
 
-function init(){
-  // our stage
-  scene = new THREE.Scene();
-  
-  // the floor lies in the xz-plane, don't worry about aspect here, will be done on resize 
-  camera = new THREE.PerspectiveCamera(60, 1 /*aspect*/, 0.1, 10000);
-  camera.position.set(0, 600, 1700);
-  
-  // the worker in the shadow
-  renderer = new THREE.WebGLRenderer();
-  document.body.appendChild(renderer.domElement);
-  
-  // showing nice fps
-  stats = new Stats();
-  stats.showPanel( 1 ); // 0: fps, 1: ms, 2: mb, 3+: custom
-  document.body.appendChild( stats.dom );
-  
-  // make it movable
-  controls = new THREE.OrbitControls(camera);
-  controls.maxPolarAngle = Math.PI/2;
-  controls.maxDistance = 2000;
-  controls.clamper = function(pos){
-    if (pos.y < 0.5) {pos.y = 0.5;} // should add clamper to protect skybox
-  };
-  
-  // handle loading
-  manager = new THREE.LoadingManager();
-  
-  // making it awesome fullwindow
-  window.addEventListener('resize', resize, false);
-  resize();
-  
-  vw.load();
-  
-  manager.onLoad = function(){
-    vw.onLoad();
-    SimPulse.init(); // currently simulation starts automatically
-    animate()
-  };
-}
+var AntMe = {};
 
-function resize() {
-  var w = document.documentElement.clientWidth;
-  var h = document.documentElement.clientHeight;
-  renderer.setSize(w, h);
-  camera.aspect = w/h;
-  camera.updateProjectionMatrix();
-}
+(function(am){
 
-function animate(e){
-  if (SimPulse.needsRedraw){
-    stats.begin();
-    renderer.render(scene, camera);
-    SimPulse.needRedraw = false;
-    stats.end();
+  // project-wide variables
+  var scene, camera, renderer, stats, controls, manager;
+
+  function init(){
+    // our stage
+    scene = new THREE.Scene();
+    
+    // the floor lies in the xz-plane, don't worry about aspect here, will be done on resize 
+    camera = new THREE.PerspectiveCamera(60, 1 /*aspect*/, 0.1, 10000);
+    camera.position.set(0, 600, 1700);
+    
+    // the worker in the shadow
+    renderer = new THREE.WebGLRenderer();
+    document.body.appendChild(renderer.domElement);
+    
+    // showing nice fps
+    stats = new Stats();
+    stats.showPanel( 1 ); // 0: fps, 1: ms, 2: mb, 3+: custom
+    document.body.appendChild( stats.dom );
+    
+    // make it movable
+    controls = new THREE.OrbitControls(camera);
+    controls.maxPolarAngle = Math.PI/2;
+    controls.maxDistance = 2000;
+    controls.clamper = function(pos){
+      if (pos.y < 0.5) {pos.y = 0.5;} // should add clamper to protect skybox
+    };
+    
+    // handle loading
+    manager = new THREE.LoadingManager();
+    
+    // making it awesome fullwindow
+    window.addEventListener('resize', resize, false);
+    resize();
+    
+    vw.load();
+    
+    manager.onLoad = function(){
+      vw.onLoad();
+      vw.onExtLoad();
+      animate()
+    };
   }
-  requestAnimationFrame(animate);
-}
 
-
-// THIS IS a 3d object manager
-function UnitStore(proto){
-  var store = {};
-  var ready = [];
-  var prefix = 'unitstorekey:';
-  
-  this.has = function(id){
-    return (prefix + id) in store;
+  function resize() {
+    var w = document.documentElement.clientWidth;
+    var h = document.documentElement.clientHeight;
+    renderer.setSize(w, h);
+    camera.aspect = w/h;
+    camera.updateProjectionMatrix();
   }
-  this.get = function(id){
-    if (this.has(id))
-      return store[prefix + id];
-    else {
-      if (ready.length > 0) {
-        var next = ready[0];
-        next.visible = true;
-        ready.splice(0, 1);
-        store[prefix + id] = next;
-        return next;
-      } else {
-        var newUnit = proto.clone();
-        scene.add(newUnit);
-        store[prefix+id] = newUnit;
-        return newUnit;
+
+  function animate(e){
+    if (vw.needsRedraw){
+      stats.begin();
+      renderer.render(scene, camera);
+      vw.needRedraw = false;
+      stats.end();
+    }
+    requestAnimationFrame(animate);
+  }
+
+
+  // THIS IS a 3d object manager
+  function UnitStore(proto){
+    var store = {};
+    var ready = [];
+    var prefix = 'unitstorekey:';
+    
+    this.has = function(id){
+      return (prefix + id) in store;
+    }
+    this.get = function(id){
+      if (this.has(id))
+        return store[prefix + id];
+      else {
+        if (ready.length > 0) {
+          var next = ready[0];
+          next.visible = true;
+          ready.splice(0, 1);
+          store[prefix + id] = next;
+          return next;
+        } else {
+          var newUnit = proto.clone();
+          scene.add(newUnit);
+          store[prefix+id] = newUnit;
+          return newUnit;
+        }
       }
     }
+    this.remove = function(id){
+      var obj = store[prefix + id];
+      obj.visible = false;
+      delete store[prefix + id];
+      ready.push(obj);
+    }
   }
-  this.remove = function(id){
-    var obj = store[prefix + id];
-    obj.visible = false;
-    delete store[prefix + id];
-    ready.push(obj);
-  }
-}
 
 
 
-// START OF 3d view related stuff
+  // START OF 3d view related stuff
 
-var vw = {
-    ant0 : undefined
-  , bug0 : undefined
-  , hill0 : undefined
-  , sugar0 : undefined
-  , apple0 : undefined
-  , gamefloor : undefined
-  , skybox : undefined
-  , antStore : undefined
-  , hillStore : undefined
-  , sugarStore : undefined
-  , appleStore : undefined
-  , bugStore : undefined
-  
-  , load : function(){
+  var vw = {
+      ant0 : undefined
+    , bug0 : undefined
+    , hill0 : undefined
+    , sugar0 : undefined
+    , apple0 : undefined
+    , gamefloor : undefined
+    , skybox : undefined
+    , antStore : undefined
+    , hillStore : undefined
+    , sugarStore : undefined
+    , appleStore : undefined
+    , bugStore : undefined
+    , needRedraw : true
+    , onExtLoad : function(){}
     
-    var objectLoader = new THREE.ObjectLoader(manager);
-    var textureLoader = new THREE.TextureLoader(manager);
+    , load : function(){
+      
+      var objectLoader = new THREE.ObjectLoader(manager);
+      var textureLoader = new THREE.TextureLoader(manager);
+      
+      // floor
+      var floorTexture = textureLoader.load( "assets/sand.jpg" );
+      floorTexture.wrapS = THREE.RepeatWrapping;
+      floorTexture.wrapT = THREE.RepeatWrapping;
+      floorTexture.repeat.set(4, 4);
+      var floorMat = new THREE.MeshBasicMaterial({color: 0x888888, side: THREE.DoubleSide, map:floorTexture});
+      vw.gamefloor = new THREE.Mesh(new THREE.PlaneGeometry(1000, 1000, 1, 1),floorMat);
+      vw.gamefloor.rotation.x = Math.PI / 2;
+      scene.add(vw.gamefloor);
     
-    // floor
-    var floorTexture = textureLoader.load( "assets/sand.jpg" );
-    floorTexture.wrapS = THREE.RepeatWrapping;
-    floorTexture.wrapT = THREE.RepeatWrapping;
-    floorTexture.repeat.set(4, 4);
-    var floorMat = new THREE.MeshBasicMaterial({color: 0x888888, side: THREE.DoubleSide, map:floorTexture});
-    vw.gamefloor = new THREE.Mesh(new THREE.PlaneGeometry(1000, 1000, 1, 1),floorMat);
-    vw.gamefloor.rotation.x = Math.PI / 2;
-    scene.add(vw.gamefloor);
-  
-    // skybox
-    var materialArray = [];
-    var posfixs = ['xpos', 'xneg', 'ypos', 'yneg', 'zpos', 'zneg'];
-    posfixs.forEach(function(val){
-      materialArray.push(new THREE.MeshBasicMaterial({
-        map:textureLoader.load('images/dawnmountain-' + val + '.png'),
-        side:THREE.BackSide}));
-    });
-    var skyboxMaterial = new THREE.MeshFaceMaterial( materialArray );
-    var skyboxGeom = new THREE.CubeGeometry( 5000, 5000, 5000, 1, 1, 1 );
-    var skybox = new THREE.Mesh( skyboxGeom, skyboxMaterial );
-    vw.skybox = skybox;
-    scene.add( skybox );
-    
-    // light it up
-    var ambient = new THREE.AmbientLight( 0x444444 );
-    scene.add( ambient );
-    var directionalLight = new THREE.DirectionalLight( 0xffeedd );
-    directionalLight.position.set( 1, 2, 3 ).normalize();
-    scene.add( directionalLight );
-  
-    // get models
-    objectLoader.load("models/ant.json", function ( obj ) {
-      obj.children[0].children.forEach(function(o){
-        o.material.color.setHex(0x0d0d0d);
+      // skybox
+      var materialArray = [];
+      var posfixs = ['xpos', 'xneg', 'ypos', 'yneg', 'zpos', 'zneg'];
+      posfixs.forEach(function(val){
+        materialArray.push(new THREE.MeshBasicMaterial({
+          map:textureLoader.load('images/dawnmountain-' + val + '.png'),
+          side:THREE.BackSide}));
       });
-      vw.ant0 = obj;
-    });
-    objectLoader.load("models/anthill.json", function ( obj ) {
-      var earthTexture = textureLoader.load( "assets/earth.jpg" );
-      var mat = new THREE.MeshPhongMaterial({color:0x999966});
-      mat.map = earthTexture;
-      obj.children[0].children[1].material = mat;
-      obj.children[0].children[0].material.color.setHex(0x000000);
-      obj.children[0].children[2].material.color.setHex(0xffffff);
-      vw.hill0 = obj;
-    });
-    objectLoader.load("models/apple.json", function ( obj ) {
-      obj.children[0].children[0].material.color.setHex(0x00cc00);
-      obj.children[0].children[1].material.color.setHex(0x66aa00);
-      obj.scale.set(2.6, 2.6, 2.6);
-      vw.apple0 = obj;
-    });
-    objectLoader.load("models/bug.json", function ( obj ) {
-      obj.children[0].children.forEach(function(o){
-        o.material.color.setHex(0x000000);
-        o.material.specular.setHex(0x00dddd);
+      var skyboxMaterial = new THREE.MeshFaceMaterial( materialArray );
+      var skyboxGeom = new THREE.CubeGeometry( 5000, 5000, 5000, 1, 1, 1 );
+      var skybox = new THREE.Mesh( skyboxGeom, skyboxMaterial );
+      vw.skybox = skybox;
+      scene.add( skybox );
+      
+      // light it up
+      var ambient = new THREE.AmbientLight( 0x444444 );
+      scene.add( ambient );
+      var directionalLight = new THREE.DirectionalLight( 0xffeedd );
+      directionalLight.position.set( 1, 2, 3 ).normalize();
+      scene.add( directionalLight );
+    
+      // get models
+      objectLoader.load("models/ant.json", function ( obj ) {
+        obj.children[0].children.forEach(function(o){
+          o.material.color.setHex(0x0d0d0d);
+        });
+        vw.ant0 = obj;
       });
-      vw.bug0 = obj;
-    });
-    objectLoader.load("models/sugar.json", function ( obj ) {
-      obj.children[0].children[0].material.color.setHex(0xffffff);
-      vw.sugar0 = obj;
-    });
+      objectLoader.load("models/anthill.json", function ( obj ) {
+        var earthTexture = textureLoader.load( "assets/earth.jpg" );
+        var mat = new THREE.MeshPhongMaterial({color:0x999966});
+        mat.map = earthTexture;
+        obj.children[0].children[1].material = mat;
+        obj.children[0].children[0].material.color.setHex(0x000000);
+        obj.children[0].children[2].material.color.setHex(0xffffff);
+        vw.hill0 = obj;
+      });
+      objectLoader.load("models/apple.json", function ( obj ) {
+        obj.children[0].children[0].material.color.setHex(0x00cc00);
+        obj.children[0].children[1].material.color.setHex(0x66aa00);
+        obj.scale.set(2.6, 2.6, 2.6);
+        vw.apple0 = obj;
+      });
+      objectLoader.load("models/bug.json", function ( obj ) {
+        obj.children[0].children.forEach(function(o){
+          o.material.color.setHex(0x000000);
+          o.material.specular.setHex(0x00dddd);
+        });
+        vw.bug0 = obj;
+      });
+      objectLoader.load("models/sugar.json", function ( obj ) {
+        obj.children[0].children[0].material.color.setHex(0xffffff);
+        vw.sugar0 = obj;
+      });
+    }
+    
+    , onLoad : function(){
+      // open stores
+      vw.antStore = new UnitStore(vw.ant0);
+      vw.hillStore = new UnitStore(vw.hill0);
+      vw.appleStore = new UnitStore(vw.apple0);
+      vw.bugStore = new UnitStore(vw.bug0);
+      vw.sugarStore = new UnitStore(vw.sugar0);
+    }
+    
+    , setAntBodyColor : function(ant, c){
+      [/*10,*/ 7, 6].forEach(function(id){
+        ant.children[0].children[id].material = new THREE.MeshPhongMaterial({color:c});
+      });
+    }
+    
+    , setHillFlagColor : function(hill, c){
+      hill.children[0].children[2].material = new THREE.MeshPhongMaterial({color:c});
+    }
+    
+    , setBugEyeColor : function(bug, color) {
+      [6, 7].forEach(function(id){
+        bug.children[0].children[id].material.color.setHex(color);
+      });
+    }
   }
   
-  , onLoad : function(){
-    // open stores
-    vw.antStore = new UnitStore(vw.ant0);
-    vw.hillStore = new UnitStore(vw.hill0);
-    vw.appleStore = new UnitStore(vw.apple0);
-    vw.bugStore = new UnitStore(vw.bug0);
-    vw.sugarStore = new UnitStore(vw.sugar0);
-  }
-  
-  , setAntBodyColor : function(ant, c){
-    [/*10,*/ 7, 6].forEach(function(id){
-      ant.children[0].children[id].material = new THREE.MeshPhongMaterial({color:c});
-    });
-  }
-  
-  , setHillFlagColor : function(hill, c){
-    hill.children[0].children[2].material = new THREE.MeshPhongMaterial({color:c});
-  }
-  
-  , setBugEyeColor : function(bug, color) {
-    [6, 7].forEach(function(id){
-      bug.children[0].children[id].material.color.setHex(color);
-    });
-  }
-}
+  // export
+  am._vw = vw;
+  am.StarteSimulation = init;
+})(AntMe);
--- src/SimPulse.js
+++ src/SimPulse.js
@@ -1,45 +1,58 @@
+"use strict";
 
 // START OF simulation pulsing: start, stop, interval, end
 
-var SimPulse = {
-    running : false
-  , nextTickTime : undefined
-  , simulationFps : 15.0
-  , fpsInterval : undefined
-  , needsRedraw : false
-  , cycles : 0
-  
-  , init:function(){
-    // call this to start simulation
-    SimPulse.needsRedraw = true;
-    SimPulse.running = true;
-    SimPulse.fpsInterval = 1000.0 / SimPulse.simulationFps;
-    SimPulse.cycles = 0;
-    Sim.init();
-    SimPulse.nextTickTime = Date.now() + SimPulse.simulationFps;
-    SimPulse.tick();
-  }
-  
-  , tick:function(){
-    if (SimPulse.cycles >= SimOpts.SimulationCycles) {
-      end();
-      return;
+(function(am, vw, Sim, SimOpts){
+
+  var SimPulse = {
+      running : false
+    , nextTickTime : undefined
+    , simulationFps : 15.0
+    , fpsInterval : undefined
+    , needsRedraw : false
+    , cycles : 0
+    
+    , init:function(){
+      // call this to start simulation
+      SimPulse.needsRedraw = true;
+      SimPulse.running = true;
+      SimPulse.fpsInterval = 1000.0 / SimPulse.simulationFps;
+      SimPulse.cycles = 0;
+      Sim.init();
+      SimPulse.nextTickTime = Date.now() + SimPulse.simulationFps;
+      SimPulse.tick();
     }
-    Sim.update();
-    SimPulse.cycles++;
-    SimPulse.needsRedraw = true;
-    if (SimPulse.running) {
-      // make sure we are really running the right fps
-      var curTime = Date.now();
-      var diff = SimPulse.nextTickTime - curTime;
-      var interval = SimPulse.fpsInterval + diff;
-      if (interval < 0) interval = 0;
-      SimPulse.nextTickTime = curTime + SimPulse.fpsInterval;
-      setTimeout(SimPulse.tick,interval);
+    
+    , tick:function(){
+      if (SimPulse.cycles >= SimOpts.SimulationCycles) {
+        end();
+        return;
+      }
+      Sim.update();
+      SimPulse.cycles++;
+      vw.needsRedraw = true;
+      if (SimPulse.running) {
+        // make sure we are really running the right fps
+        var curTime = Date.now();
+        var diff = SimPulse.nextTickTime - curTime;
+        var interval = SimPulse.fpsInterval + diff;
+        if (interval < 0) interval = 0;
+        SimPulse.nextTickTime = curTime + SimPulse.fpsInterval;
+        setTimeout(SimPulse.tick,interval);
+      }
+    }
+    
+    , end:function(){
+      SimPulse.running = false;
     }
   }
   
-  , end:function(){
-    SimPulse.running = false;
-  }
-}
+  vw.onExtLoad = SimPulse.init;
+  
+  
+  // FINAL PLACE TO SEAL
+  delete am._vw;
+  delete am._sim;
+  delete am._simOpts;
+
+})(AntMe, AntMe._vw, AntMe._sim, AntMe._simOpts);
